22b19dd7327d360580ddfdf7022a2bc1d99dbfa4
TaegyuHan <gksxorb147@gmail.com>
2024-05-13 11:06:39 +0900

    Add: #1 Hello World

---
+++ src/implementation/p2557/Main.java
@@ -0,0 +1,15 @@
+package implementation.p2557;
+
+
+/**
+ * URL : <a href="https://www.acmicpc.net/problem/2557">문제 Link</a> <br>
+ * 문제명 : Hello World
+ * <p>
+ * BigO : O(1) <br>
+ * 문제의 내용 : 백준 문제의 입문을 알리는 문제 입니다.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
