859e1c50b30d92c6f54b034bc6c258506252fb3d
TaegyuHan <gksxorb147@gmail.com>
2024-05-20 11:05:31 +0900

    Add: #8 백준 10172번 개

---
+++ src/implementation/p10171/Main.java
@@ -0,0 +1,13 @@
+package implementation.p10171;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\").append("\n")
+          .append(" )  ( ')").append("\n")
+          .append("(  /  )").append("\n")
+          .append(" \\(__)|").append("\n");
+
+        System.out.println(sb);
+    }
+}
