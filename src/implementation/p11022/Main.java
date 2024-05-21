acfc43f13d7894547b7ce5660929f15b0dddc954
TaegyuHan <gksxorb147@gmail.com>
2024-05-21 13:50:34 +0900

    Add: #8 백준 2562번 최댓값

---
+++ src/implementation/p11022/Main.java
@@ -0,0 +1,47 @@
+package implementation.p11022;
+
+import implementation.Implementation;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream(Implementation.PATH + "/p11022/data/1.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Max max = new Max();
+        for (int i = 1; i <= 9; ++i) {
+            max.setMax(i, br.readLine());
+        }
+        max.answer();
+    }
+}
+
+class Max {
+    private int max;
+    private int maxIndex;
+
+    public Max() {
+        this.max = Integer.MIN_VALUE;
+        this.maxIndex = -1;
+    }
+
+    public void setMax(int index, String data) {
+        setMax(index, Integer.parseInt(data));
+    }
+
+    public void setMax(int index, int value) {
+        if (value > max) {
+            max = value;
+            maxIndex = index;
+        }
+    }
+
+    public void answer() {
+        System.out.println(max);
+        System.out.print(maxIndex);
+    }
+}
